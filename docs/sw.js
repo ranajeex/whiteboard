a7723da72a96c00538d0c35ff4fb03454bb2d758
Niraj Prasad <niraj.prasad@gatewaygroup.com>
2019-06-05 11:14:00 +0530

    first commit

---
+++ docs/sw.js
@@ -0,0 +1,15 @@
+importScripts("./precache-manifest.801d373569076d03ea19fd9d01a7e732.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+self.addEventListener('push', (event) => {
+    const title = 'Get Started With Workbox';
+    const options = {
+        body: event.data.text(),
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
